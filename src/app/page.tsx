6eadd128cbeaad4e097f7d096f3802a402d8db69
Elle Nash <56604081+v0idx@users.noreply.github.com>
2025-07-30 14:26:09 +0100

    made the site

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,40 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 
+import { TwitterLogoIcon, ButterflyIcon, LastfmLogoIcon, GoogleChromeLogoIcon, SmileyIcon, TumblrLogoIcon} from "@phosphor-icons/react/ssr";
+
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+    <div className={styles.page}>      
+      <div className={styles.infoPanel}>
+        <div className="imageContainer">
+          <img className="profileImage" src="lily.webp" />
         </div>
+        <div className="infoText">
+          <h1>Eloise</h1>
+          <p>Developer |  Student  | Gremlin</p>
+          <br/>
+          <hr/>
+          <p>Nord Colour Theme Enthusiast</p> 
+        </div>
+         
+      </div>
+      <main className={styles.main}>
+        
+        <h1 className="mainTitle">things that i have</h1>
+        <ol>
+          <li><a href="https://x.com/v0idx_"><TwitterLogoIcon /> my twitter</a></li>
+          <li><a href="https://bsky.app/profile/v0idx.com"><ButterflyIcon /> my bluesky</a></li>
+          <li><a href="https://v0idx.com"><GoogleChromeLogoIcon /> my website!!</a></li>
+          <li><a href="https://en.pronouns.page/@v0idx"><SmileyIcon /> pronouns page</a></li>
+          <li><a href="https://last.fm/user/v0idx"><LastfmLogoIcon /> last.fm!! see what i'm listening to</a></li>
+        </ol>
+        <h1>i write abt music (sometimes)</h1>
+        <ol>
+          <li><a href="https://tumblr.com/lily-likes-music"><TumblrLogoIcon /> da blog</a></li>
+        </ol>
       </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      
     </div>
   );
 }
