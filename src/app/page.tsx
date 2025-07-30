281f63a8b1996487c93738b20ebec07a750f1e4e
Elle Nash <56604081+v0idx@users.noreply.github.com>
2025-07-30 14:34:59 +0100

    added favicon, housekeeping

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import styles from "./page.module.css";
 
 import { TwitterLogoIcon, ButterflyIcon, LastfmLogoIcon, GoogleChromeLogoIcon, SmileyIcon, TumblrLogoIcon} from "@phosphor-icons/react/ssr";
